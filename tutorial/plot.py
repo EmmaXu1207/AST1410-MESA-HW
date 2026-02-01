e533f645b9569b410ca575c157521db00966686a
EmmaXu1207 <emmajiayixu1207@gmail.com>
2026-02-01 18:44:50 -0500

    Add files via upload
    Homework 1: installation and reproduce the default plot

---
+++ tutorial/plot.py
@@ -0,0 +1,47 @@
+import mesa_reader as mr
+import matplotlib.pyplot as plt
+
+# A simple plotting script for MESA data with mesa_reader
+# mesa_reader offers a lot of functionality!
+# Check out the documentation at:
+# https://billwolf.space/py_mesa_reader/
+
+# Specify the directory containing MESA logs
+logs = mr.MesaLogDir("LOGS")
+
+# Read the history and profile data
+history = logs.history
+profile = logs.profile_data(model_number=-1)  # -1 for the last model
+
+# Make an HR diagram from the history data
+plt.figure()
+plt.plot(history.log_Teff, history.log_L)
+plt.xlabel(r"$\log_{10}(T_{\mathrm{eff}})\ \mathrm{[K]}$")
+plt.ylabel(r"$\log_{10}(L)\ [L_\odot]$")
+plt.gca().invert_xaxis()
+plt.savefig('log_Teff-log_L')
+
+# Plot the density profile as a function of the mass coordinate
+plt.figure()
+plt.plot(profile.mass, profile.Rho)
+plt.xlabel(r"$M(r)\ [M_\odot]$")
+plt.ylabel(r"$\rho\ [\mathrm{g/cm}^3]$")
+plt.yscale("log")
+plt.savefig('Mass-density')
+
+plt.figure()
+plt.plot(profile.Rho, profile.T)
+plt.yscale('log')
+plt.xscale('log')
+plt.xlabel(r"$\rho\ [\mathrm{g/cm}^3]$")
+plt.ylabel(r"$\log_{10}(T_{\mathrm{eff}})\ \mathrm{[K]}$")
+plt.savefig('density-temperature')
+
+plt.figure()
+plt.plot(profile.Rho,profile.P)
+plt.xlabel(r"$\rho\ [\mathrm{g/cm}^3]$")
+plt.ylabel(r"$P\ [\mathrm{dyn/cm^2}]$")
+plt.savefig('density-pressure')
+
+# Show plots
+plt.show()
